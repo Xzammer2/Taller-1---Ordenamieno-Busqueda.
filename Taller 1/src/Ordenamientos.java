1500dbaf9314c6f16bfd99cb48f0f2d558f14f85
Xzammer2 <sergio2252875@correo.uis.edu.co>
2026-08-21 11:48:07 -0500

    Adelante todos los metodos, faltan retoques

--- Taller 1/src/Ordenamientos.java
+++
@@ -1,7 +0,0 @@
-public interface Ordenamientos {
-    int[] bubbleSort(int[] arrayDesordenado);
-    int[] selectionSort(int[] arrayDesordenado);
-    int[] insertionSort(int[] arrayDesordenado);
-    int[] mergeSort(int[] arrayDesordenado);
-    int[] quickSort(int[] arrayDesordenado);
-}
